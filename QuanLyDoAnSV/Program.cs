b23d4f2646a67ed0eb534008706e8c14a48f91a0
ASUS <ASUS@DESKTOP-26R9BRI>
2021-11-10 20:52:16 +0700

    Xóa Form1 cho đỡ rối

--- QuanLyDoAnSV/Program.cs
+++ QuanLyDoAnSV/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyDoAnSV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Mainform());
+            Application.Run(new Hoang.frmLogin());
         }
     }
 }
